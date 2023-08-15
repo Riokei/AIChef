567c527f18b96851f23d8a32d4b2a78284aac69c
Chris <rikoukuroi@yahoo.com>
2023-08-15 18:35:07 -0400

    Added mock data and begun laying down api call.

---
+++ Server/Controllers/RecipeController.cs
@@ -0,0 +1,19 @@
+﻿using AIChef.Server.Data;
+using AIChef.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIChef.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        [HttpPost, Route("GetRecipeIdeas")]
+        public async Task<ActionResult<List<Idea>>> GetRecipeIdeas(RecipeParms recipeParms)
+        {
+            return SampleData.RecipeIdeas;
+        }
+
+    }
+}
