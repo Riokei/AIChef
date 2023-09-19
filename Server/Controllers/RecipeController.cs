144f0ee5a28378985bfd9f557dd6016284480402
Chris <rikoukuroi@yahoo.com>
2023-09-19 19:05:03 -0400

    Beginning work of recipe page generation.

--- Server/Controllers/RecipeController.cs
+++ Server/Controllers/RecipeController.cs
@@ -18,19 +18,29 @@ namespace AIChef.Server.Controllers
         [HttpPost, Route("GetRecipeIdeas")]
         public async Task<ActionResult<List<Idea>>> GetRecipeIdeas(RecipeParms recipeParms)
         {
-            string mealtime = recipeParms.MealTime!;
-            List<string> ingredients = recipeParms.Ingredients!
-                .Where(x => !string.IsNullOrEmpty(x.Description!))
-                .Select(x => x.Description!)
-                .ToList();
-            if(string.IsNullOrEmpty(mealtime))
-            {
-                mealtime = "Breakfast";
-            }
-            var ideas = await _openAiService.CreateRecipeIdeas(mealtime, ingredients);
+            //string mealtime = recipeParms.MealTime!;
+            //List<string> ingredients = recipeParms.Ingredients!
+            //    .Where(x => !string.IsNullOrEmpty(x.Description!))
+            //    .Select(x => x.Description!)
+            //    .ToList();
+            //if(string.IsNullOrEmpty(mealtime))
+            //{
+            //    mealtime = "Breakfast";
+            //}
+            //var ideas = await _openAiService.CreateRecipeIdeas(mealtime, ingredients);
 
-            return ideas;
-            //return SampleData.RecipeIdeas;
+            //return ideas;
+            return SampleData.RecipeIdeas;
+        }
+        [HttpPost, Route("GetRecipe")]
+        public async Task<ActionResult<Recipe?>> GetRecipe(RecipeParms recipeParms)
+        {
+            return SampleData.Recipe;
+        }
+        [HttpGet, Route("GetRecipeImage")]
+        public async Task<RecipeImage> GetRecipeImage(string title)
+        {
+            return SampleData.RecipeImage;
         }
 
     }
